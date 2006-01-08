e8aaae3412579c3cfd61ca98df33fd74a96fda08
gianasista <gianasista@fd1bd6e1-2897-4527-8272-f809ea2bdf6a>
2006-01-08 19:54:08 +0000

    *** empty log message ***

---
+++ moreunit/src/moreUnit/preferences/MoreUnitPreferencePage.java
@@ -0,0 +1,30 @@
+package moreUnit.preferences;
+
+import moreUnit.MoreUnitPlugin;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * @author vera
+ * 08.01.2006 19:24:23
+ */
+public class MoreUnitPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage{
+
+	public MoreUnitPreferencePage() {
+		super(FieldEditorPreferencePage.FLAT);
+	}
+	
+	protected void createFieldEditors() {
+		StringFieldEditor junitDirPreferenceField = new StringFieldEditor(MoreUnitPlugin.JUNIT_PATH_PREFERENCE, "Directory for testcases", 10, getFieldEditorParent());
+		addField(junitDirPreferenceField);
+	}
+
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(MoreUnitPlugin.getDefault().getPreferenceStore());
+	}
+
+
+}
