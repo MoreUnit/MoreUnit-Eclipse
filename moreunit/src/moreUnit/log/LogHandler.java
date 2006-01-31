cae9851b9b0798be408857f9c45272ae68b647b4
gianasista <gianasista@fd1bd6e1-2897-4527-8272-f809ea2bdf6a>
2006-01-31 21:35:04 +0000

    Small changes of the log string.

--- moreunit/src/moreUnit/log/LogHandler.java
+++ moreunit/src/moreUnit/log/LogHandler.java
@@ -15,20 +15,23 @@ public class LogHandler {
 	}
 
 	public void handleExceptionLog(Exception exception) {
-		System.out.println("moreunit: (EXC)"+ exception.getMessage());
+		System.out.println("moreunit: (EXC) "+ exception.getMessage());
 		exception.printStackTrace();
 	}
 	
 	public void handleInfoLog(String infoMessage) {
-		System.out.println("moreunit: (INFO)"+infoMessage);
+		System.out.println("moreunit: (INFO) "+infoMessage);
 	}
 	
 	public void handleWarnLog(String warning) {
-		System.out.println("moreunit (WARN):"+warning);
+		System.out.println("moreunit (WARN) :"+warning);
 	}
 }
 
 // $Log: not supported by cvs2svn $
+// Revision 1.3  2006/01/31 19:36:46  gianasista
+// Add logging for warnings.
+//
 // Revision 1.2  2006/01/19 21:39:44  gianasista
 // Added CVS-commit-logging to all java-files
 //
