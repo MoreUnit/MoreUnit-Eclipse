d5ff6397ab077b0b1cd9e46d84d1347e770d2b01
gianasista <gianasista@fd1bd6e1-2897-4527-8272-f809ea2bdf6a>
2006-02-01 20:57:39 +0000

    New testmethod for createTestCase

--- moreunit/junit/moreUnit/elements/JavaFileFacadeTest.java
+++ moreunit/junit/moreUnit/elements/JavaFileFacadeTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import moreUnit.TestProject;
 import moreUnit.util.MagicNumbers;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
@@ -22,15 +23,6 @@ public class JavaFileFacadeTest extends TestCase {
 		
 		testProject = new TestProject("ProjektJavaFileFacade");
 		testProject.createPackage("com");
-		
-		IPackageFragment comPaket = testProject.createPackage("com");
-		IType helloType = testProject.createType(comPaket, "Hello.java", getJavaSource());
-		javaFileFacade = new JavaFileFacade(helloType.getCompilationUnit());
-		
-		junitSourceRoot = testProject.createAdditionalSourceFolder("junit");
-		IPackageFragment junitComPaket = testProject.createPackage(junitSourceRoot, "com");
-		IType testHelloType = testProject.createType(junitComPaket, "HelloTest.java", getTestSource());
-		testJavaFileFacade = new JavaFileFacade(testHelloType.getCompilationUnit());
 	}
 	
 	protected void tearDown() throws Exception {
@@ -39,16 +31,46 @@ public class JavaFileFacadeTest extends TestCase {
 		testProject.dispose();
 	}
 
-	public void testGetCorrespondingTestCase() {
+	public void testGetCorrespondingTestCase() throws CoreException {
+		IPackageFragment comPaket = testProject.createPackage("com");
+		IType helloType = testProject.createType(comPaket, "Hello.java", getJavaSource());
+		javaFileFacade = new JavaFileFacade(helloType.getCompilationUnit());
+		
+		junitSourceRoot = testProject.createAdditionalSourceFolder("junit");
+		IPackageFragment junitComPaket = testProject.createPackage(junitSourceRoot, "com");
+		IType testHelloType = testProject.createType(junitComPaket, "HelloTest.java", getTestSource());
+		testJavaFileFacade = new JavaFileFacade(testHelloType.getCompilationUnit());		
+		
 		IType testCaseType = javaFileFacade.getCorrespondingTestCase();
 		
 		assertNotNull(testCaseType);
 		assertEquals("com.HelloTest", testCaseType.getFullyQualifiedName());
 	}
 	
-	public void testIsTestCase() {
+	public void testIsTestCase() throws CoreException {
+		IPackageFragment comPaket = testProject.createPackage("com");
+		IType helloType = testProject.createType(comPaket, "Hello.java", getJavaSource());
+		javaFileFacade = new JavaFileFacade(helloType.getCompilationUnit());
+		
+		junitSourceRoot = testProject.createAdditionalSourceFolder("junit");
+		IPackageFragment junitComPaket = testProject.createPackage(junitSourceRoot, "com");
+		IType testHelloType = testProject.createType(junitComPaket, "HelloTest.java", getTestSource());
+		testJavaFileFacade = new JavaFileFacade(testHelloType.getCompilationUnit());
+		
 		assertTrue(testJavaFileFacade.isTestCase());
 		assertFalse(javaFileFacade.isTestCase());
+	}
+	
+	public void testCreateTestCase() throws CoreException {
+		IPackageFragment comPaket = testProject.createPackage("com");
+		IType wowType = testProject.createType(comPaket, "Wow.java", getJavaSource2());
+		javaFileFacade = new JavaFileFacade(wowType.getCompilationUnit());
+		
+		junitSourceRoot = testProject.createAdditionalSourceFolder("junit");
+		
+		IType testCaseType = javaFileFacade.createTestCase();
+		assertNotNull(testCaseType);
+		assertEquals("com.WowTest", testCaseType.getFullyQualifiedName());
 	}
 	
 	private String getJavaSource() {
@@ -56,6 +78,16 @@ public class JavaFileFacadeTest extends TestCase {
 		source.append("package com;").append(MagicNumbers.NEWLINE);
 		source.append("public class Hello {").append(MagicNumbers.NEWLINE);
 		source.append("public int getOne() { return 1; }").append(MagicNumbers.NEWLINE);
+		source.append("}");
+		
+		return source.toString();
+	}
+	
+	private String getJavaSource2() {
+		StringBuffer source = new StringBuffer();
+		source.append("package com;").append(MagicNumbers.NEWLINE);
+		source.append("public class Wow {").append(MagicNumbers.NEWLINE);
+		source.append("public String getOneString() { return \"1\"; }").append(MagicNumbers.NEWLINE);
 		source.append("}");
 		
 		return source.toString();
@@ -76,3 +108,6 @@ public class JavaFileFacadeTest extends TestCase {
 }
 
 // $Log$
+// Revision 1.1  2006/01/31 21:33:32  gianasista
+// Started writing the first tests for this class.
+//
