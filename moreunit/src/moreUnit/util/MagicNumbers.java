64d4ca72aa67e31477d96ef9c2fdf4e02c834a1f
gianasista <gianasista@fd1bd6e1-2897-4527-8272-f809ea2bdf6a>
2006-05-16 20:15:56 +0000

    new constant for java file extension

--- moreunit/src/moreUnit/util/MagicNumbers.java
+++ moreunit/src/moreUnit/util/MagicNumbers.java
@@ -12,12 +12,17 @@ public class MagicNumbers {
 	//public static final String TEST_CASE_SUFFIX = "Test";
 	public static final String TEST_METHOD_PRAEFIX = "test";
 	
+	public static final String JAVA_FILE_EXTENSION = ".java";
+	
 	public static final String STRING_DOT = ".";
 	public static final String EMPTY_STRING = "";
 	public static final String NEWLINE = "\n";
 }
 
 // $Log: not supported by cvs2svn $
+// Revision 1.3  2006/04/14 17:11:56  gianasista
+// Suffix for testcasename ist configurable (+Tests)
+//
 // Revision 1.2  2006/01/19 21:39:44  gianasista
 // Added CVS-commit-logging to all java-files
 //
