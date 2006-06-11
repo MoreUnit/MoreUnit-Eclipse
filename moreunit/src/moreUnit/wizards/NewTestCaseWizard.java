101e602e5ac285ebb5f4c3311dbc0758f7a8c767
gianasista <gianasista@fd1bd6e1-2897-4527-8272-f809ea2bdf6a>
2006-06-11 20:09:04 +0000

    added package prefix preference and support for eclipse 3.1.x

--- moreunit/src/moreUnit/wizards/NewTestCaseWizard.java
+++ moreunit/src/moreUnit/wizards/NewTestCaseWizard.java
@@ -1,13 +1,16 @@
 package moreUnit.wizards;
 
 import moreUnit.elements.JavaProjectFacade;
+import moreUnit.log.LogHandler;
 import moreUnit.preferences.Preferences;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.junit.wizards.NewTestCaseWizardPageOne;
 import org.eclipse.jdt.junit.wizards.NewTestCaseWizardPageTwo;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -31,8 +34,13 @@ public class NewTestCaseWizard extends NewClassyWizard {
 		pageOne.setWizard(this);
 		pageTwo.setWizard(this);
 		pageOne.init(new StructuredSelection(getType()));
-		//initialisePackageFragment();
+		initialisePackageFragment();
+		
+		// Eclipse 3.1.x  does not support junit 4
+		try {
 		pageOne.setJUnit4(Preferences.instance().shouldUseJunit4Type(), true);
+		} catch (NoSuchMethodError error) {
+		}
 		pageOne.setPackageFragmentRoot(getPackageFragmentRootFromSettings(), true);
 		addPage(pageOne);
 		addPage(pageTwo);
@@ -43,21 +51,22 @@ public class NewTestCaseWizard extends NewClassyWizard {
 		return (new JavaProjectFacade(project)).getJUnitSourceFolder();
 	}
 
-//	private void initialisePackageFragment() {
-//		if (!Preferences.instance().hasTestPackagePrefix()) {
-//			return;
-//		}
-//		try {
-//			IPackageFragment packageFragment = createPackageFragment(getPackageFragmentNameWithPrefix());
-//			pageOne.setPackageFragment(packageFragment, true);
-//		} catch (JavaModelException e) {
-//			MoreUnitPlugin.getDefault().log("Unable to create package fragment root", e);
-//		}
-//	}
-//
-//	private String getPackageFragmentNameWithPrefix() {
-//		return Preferences.instance().getTestPackagePrefix() + "." + pageOne.getPackageFragment().getElementName();
-//	}
+	private void initialisePackageFragment() {
+		String testPackagePrefix = Preferences.instance().getTestPackagePrefix();
+		if (testPackagePrefix == null || testPackagePrefix.length() == 0) {
+			return;
+		}
+		try {
+			IPackageFragment packageFragment = createPackageFragment(getPackageFragmentNameWithPrefix());
+			pageOne.setPackageFragment(packageFragment, true);
+		} catch (JavaModelException e) {
+			LogHandler.getInstance().handleWarnLog("Unable to create package fragment root");
+		}
+	}
+
+	private String getPackageFragmentNameWithPrefix() {
+		return Preferences.instance().getTestPackagePrefix() + "." + pageOne.getPackageFragment().getElementName();
+	}
 
 	public IType createClass() throws CoreException, InterruptedException {
 		pageOne.createType(new NullProgressMonitor());
@@ -70,6 +79,9 @@ public class NewTestCaseWizard extends NewClassyWizard {
 }
 
 // $Log$
+// Revision 1.2  2006/05/31 19:40:23  gianasista
+// Preferences are used for initialization of the wizard
+//
 // Revision 1.1  2006/05/12 22:33:41  channingwalton
 // added class creation wizards if type to jump to does not exist
 //
