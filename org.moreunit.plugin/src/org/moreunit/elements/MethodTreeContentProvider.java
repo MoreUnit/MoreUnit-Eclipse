edd919142e4d9e4ba76910de546471c9a7e71143
channingwalton <channingwalton@fd1bd6e1-2897-4527-8272-f809ea2bdf6a>
2007-11-20 06:23:27 +0000

    fixed a NullPointerException and added the qualifier to the plugin id

--- org.moreunit.plugin/src/org/moreunit/elements/MethodTreeContentProvider.java
+++ org.moreunit.plugin/src/org/moreunit/elements/MethodTreeContentProvider.java
@@ -15,15 +15,15 @@ import org.moreunit.ui.MethodPage;
 
 /**
  * @author vera
- *
+ * 
  */
 public class MethodTreeContentProvider implements ITreeContentProvider {
-	
+
 	IType classType;
 	List<IMethod> methods = new ArrayList<IMethod>();
 	private boolean isPrivateFiltered = false;
 	private boolean isGetterFiltered = false;
-	
+
 	public MethodTreeContentProvider(IType javaFileFile) {
 		this.classType = javaFileFile;
 		resetMethods(javaFileFile);
@@ -31,13 +31,16 @@ public class MethodTreeContentProvider implements ITreeContentProvider {
 
 	private void resetMethods(IType javaFileFile) {
 		methods = new ArrayList<IMethod>();
-		if(!TypeFacade.isTestCase(javaFileFile))
+		if (javaFileFile == null) {
+			return;
+		}
+		if (!TypeFacade.isTestCase(javaFileFile))
 			try {
 				ClassTypeFacade typeFacade = new ClassTypeFacade(javaFileFile.getCompilationUnit());
 				IMethod[] allMethods = javaFileFile.getMethods();
-				
+
 				for (IMethod method : allMethods) {
-					if(typeFacade.getCorrespondingTestMethods(method).size() == 0)
+					if (typeFacade.getCorrespondingTestMethods(method).size() == 0)
 						methods.add(method);
 				}
 			} catch (JavaModelException e) {
@@ -58,19 +61,19 @@ public class MethodTreeContentProvider implements ITreeContentProvider {
 	}
 
 	public Object[] getElements(Object inputElement) {
-		if(inputElement instanceof MethodPage)
-			resetMethods(((MethodPage)inputElement).getInputType());
-		
+		if (inputElement instanceof MethodPage)
+			resetMethods(((MethodPage) inputElement).getInputType());
+
 		List<IMethod> resultMethodList = new ArrayList<IMethod>();
-		if(isPrivateFiltered)
+		if (isPrivateFiltered)
 			resultMethodList.addAll(filterPrivateMethods(methods));
 		else
 			resultMethodList.addAll(methods);
-		
-		if(isGetterFiltered) {
+
+		if (isGetterFiltered) {
 			return filterGetterAndSetter(resultMethodList).toArray();
 		}
-		
+
 		return resultMethodList.toArray();
 	}
 
@@ -79,29 +82,29 @@ public class MethodTreeContentProvider implements ITreeContentProvider {
 	}
 
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
- 	}
-	
+	}
+
 	private List<IMethod> filterPrivateMethods(List<IMethod> methodList) {
 		List<IMethod> resultList = new ArrayList<IMethod>();
 		FilterMethodVisitor privateMethodVisitor = new FilterMethodVisitor(classType);
-		
+
 		for (IMethod method : methodList) {
-			if(!privateMethodVisitor.isPrivateMethod(method))
+			if (!privateMethodVisitor.isPrivateMethod(method))
 				resultList.add(method);
 		}
-		
+
 		return resultList;
 	}
-	
+
 	private List<IMethod> filterGetterAndSetter(List<IMethod> methodList) {
 		List<IMethod> resultList = new ArrayList<IMethod>();
 		FilterMethodVisitor getterMethodVisitor = new FilterMethodVisitor(classType);
-		
-		for(IMethod method : methodList) {
-			if(!getterMethodVisitor.isGetterMethod(method) && !getterMethodVisitor.isSetterMethod(method))
+
+		for (IMethod method : methodList) {
+			if (!getterMethodVisitor.isGetterMethod(method) && !getterMethodVisitor.isSetterMethod(method))
 				resultList.add(method);
 		}
-		
+
 		return resultList;
 	}
 
