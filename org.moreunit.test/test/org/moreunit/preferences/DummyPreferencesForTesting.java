13fcd3904cf8e181ff36d20d488f8b9cf90c810f
(no author) <(no author)@fd1bd6e1-2897-4527-8272-f809ea2bdf6a>
2009-03-23 19:20:44 +0000

    This commit was manufactured by cvs2svn to create tag 'V_1_3_2'.

--- org.moreunit.test/test/org/moreunit/preferences/DummyPreferencesForTesting.java
+++
@@ -1,9 +0,0 @@
-package org.moreunit.preferences;
-
-public class DummyPreferencesForTesting extends Preferences
-{
-	public DummyPreferencesForTesting()
-	{
-		Preferences.setInstance(this);
-	}
-}
