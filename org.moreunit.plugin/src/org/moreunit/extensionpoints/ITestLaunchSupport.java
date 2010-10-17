1a9b13cbcd3ae704a77ee7f18e71536382eccb70
(no author) <(no author)@fd1bd6e1-2897-4527-8272-f809ea2bdf6a>
2010-10-17 19:36:39 +0000

    This commit was manufactured by cvs2svn to create tag 'V_2_2_0'.

--- org.moreunit.plugin/src/org/moreunit/extensionpoints/ITestLaunchSupport.java
+++ org.moreunit.plugin/src/org/moreunit/extensionpoints/ITestLaunchSupport.java
@@ -4,11 +4,6 @@ import org.eclipse.debug.ui.ILaunchShortcut;
 import org.eclipse.jdt.core.IJavaElement;
 import org.moreunit.preferences.PreferenceConstants;
 
-/**
- * This interface should be implemented to provide additional support for
- * launching tests of a TestType that is supported by MoreUnit (JUnit3, JUnit4
- * and TestNG at the time of this writing).
- */
 public interface ITestLaunchSupport
 {
 
@@ -51,24 +46,7 @@ public interface ITestLaunchSupport
         }
     }
 
-    /**
-     * Returns whether this instance supports launching test element(s) for the
-     * given test framework, being of the given {@link IJavaElement}'s sub-type
-     * and with the given cardinality.
-     * 
-     * @param testType the test framework (JUnit3, JUnit4, TestNG...) used by
-     *            the test element(s)
-     * @param elementType the {@link IJavaElement} type or sub-type of the test
-     *            elements to be launched
-     * @param cardinality the number of test elements to be launched:
-     *            <code>ONE</code> or <code>SEVERAL</code>
-     * @return <code>true</code> if the given configuration is supported,
-     *         <code>false</code> otherwise
-     */
     boolean isLaunchSupported(TestType testType, Class< ? extends IJavaElement> elementType, Cardinality cardinality);
 
-    /**
-     * Returns the {@link ILaunchShortcut} provided by this instance.
-     */
     ILaunchShortcut getShortcut();
 }
