d707c3e8743c5967f6087ef65e60c702fce25202
gianasista <gianasista@users.sourceforge.net>
2012-01-17 21:22:37 +0100

    Another test to check @Properties

--- org.moreunit.test/test/org/moreunit/util/PreferencesPropertiesTest.java
+++ org.moreunit.test/test/org/moreunit/util/PreferencesPropertiesTest.java
@@ -1,6 +1,8 @@
 package org.moreunit.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.moreunit.test.context.ContextTestCase;
@@ -13,10 +15,20 @@ public class PreferencesPropertiesTest extends ContextTestCase
 {
     @Project(mainCls="SomeClass", properties = @Properties(testClassPrefixes="tseT"))
     @Test
-    public void testProperties()
+    public void testWithProperties()
     {
-        String[] prefixes = org.moreunit.preferences.Preferences.getInstance().getPrefixes(context.getProjectHandler().get());
+        org.moreunit.preferences.Preferences prefs = org.moreunit.preferences.Preferences.getInstance();
+        assertTrue(prefs.hasProjectSpecificSettings(context.getProjectHandler().get()));
+        String[] prefixes = prefs.getPrefixes(context.getProjectHandler().get());
         assertEquals(1, prefixes.length);
         assertEquals("tseT", prefixes[0]);
+    }
+    
+    @Project(mainCls="SomeClass")
+    @Test
+    public void testWithoutProperties()
+    {
+        org.moreunit.preferences.Preferences prefs = org.moreunit.preferences.Preferences.getInstance();
+        assertFalse(prefs.hasProjectSpecificSettings(context.getProjectHandler().get()));
     }
 }
