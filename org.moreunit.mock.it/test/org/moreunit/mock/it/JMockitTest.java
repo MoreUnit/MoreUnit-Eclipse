9b76b0df87c0b172faf701ffbf79d0dd44dde40e
Nicolas Demengel <nicolas.demengel@gmail.com>
2012-12-06 16:50:12 +0100

    Adds templates to generate code for JMockit

---
+++ org.moreunit.mock.it/test/org/moreunit/mock/it/JMockitTest.java
@@ -0,0 +1,17 @@
+package org.moreunit.mock.it;
+
+import org.moreunit.test.context.Context;
+import org.moreunit.test.context.Preferences;
+import org.moreunit.test.context.TestType;
+
+@Context(mainSrc = "SomeConcept.cut.java.txt",
+        testSrc = "SomeConcept.test.java.txt",
+        preferences = @Preferences(testType = TestType.JUNIT4,
+                testClassSuffixes = "Test"))
+public class JMockitTest extends MockingTestCase
+{
+    public JMockitTest()
+    {
+        super("JMockit", "org.moreunit.mock.jmockitDefault");
+    }
+}
