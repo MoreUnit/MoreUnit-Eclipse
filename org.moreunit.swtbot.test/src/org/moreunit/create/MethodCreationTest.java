79861824e9b92f4967504fa8530c19861d34a942
Aurélien Pupier <apupier@redhat.com>
2017-07-28 15:28:46 +0200

    #41 - Use correct Java project for class under test
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- org.moreunit.swtbot.test/src/org/moreunit/create/MethodCreationTest.java
+++ org.moreunit.swtbot.test/src/org/moreunit/create/MethodCreationTest.java
@@ -13,6 +13,7 @@ import org.moreunit.ConditionCursorLine;
 import org.moreunit.JavaProjectSWTBotTestHelper;
 import org.moreunit.test.context.Project;
 import org.moreunit.test.context.Properties;
+import org.moreunit.test.context.TestProject;
 import org.moreunit.test.context.TestType;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
@@ -96,4 +97,58 @@ public class MethodCreationTest extends JavaProjectSWTBotTestHelper
 		assertThat(methods).hasSize(2).onProperty("elementName").contains("testGetNumber1Suffix");
 	}
 	
+	@Project(mainSrc = "MethodCreation_class_with_method.txt",
+	        mainSrcFolder = "src",
+	        testProject = @TestProject(src = "MethodCreation_test_without_method.txt"),
+	        properties = @Properties(
+	                                 testType = TestType.JUNIT4,
+	                                 testClassNameTemplate = "${srcFile}Test",
+	                                 testMethodPrefix = true))
+	@Test
+	public void should_create_testmethod_when_shortcut_is_pressed_in_method_with_test_folder_in_another_project() throws JavaModelException
+	{
+	    openResource("TheWorld.java");
+	    SWTBotEclipseEditor cutEditor = bot.activeEditor().toTextEditor();
+	    // move cursor to method
+	    int lineNumberOfMethod = 6;
+	    cutEditor.navigateTo(lineNumberOfMethod, 9);
+	    bot.waitUntil(new ConditionCursorLine(cutEditor, lineNumberOfMethod));
+
+	    getShortcutStrategy().pressGenerateShortcut();
+	    
+	    // adding the method to the testcase takes a short moment
+	    bot.waitUntil(new DefaultCondition()
+        {
+            
+            @Override
+            public boolean test() throws Exception
+            {
+                IMethod[] methods = context.getCompilationUnit("testing.TheWorldTest").findPrimaryType().getMethods();
+                return methods.length == 1;
+            }
+            
+            @Override
+            public String getFailureMessage()
+            {
+                try
+                {
+                    IMethod[] methods = context.getCompilationUnit("testing.TheWorldTest").findPrimaryType().getMethods();
+                    StringBuilder errorMessage = new StringBuilder("Element names of methods found: ");
+                    for (IMethod method : methods)
+                    {
+                        errorMessage.append(method.getElementName());
+                    }
+                    return errorMessage.toString();
+                }
+                catch (JavaModelException e)
+                {
+                    return "Cannot retrieve methods " + e.getMessage();
+                }
+            }
+        }, 20000);
+	    
+        IMethod[] methods = context.getCompilationUnit("testing.TheWorldTest").findPrimaryType().getMethods();
+        assertThat(methods).onProperty("elementName").containsOnly("testGetNumber1");
+	}
+
 }
