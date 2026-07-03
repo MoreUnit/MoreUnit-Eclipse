71a5e27e3e913ba06c927d5f8a4e3fa266423fd6
Hélios GILLES <helios.gilles@gmail.com>
2026-07-03 22:04:55 +0200

    test: add LogHandler and TypeFacadeFactory tests

---
+++ org.moreunit.mock.test/test/org/moreunit/mock/elements/TypeFacadeFactoryTest.java
@@ -0,0 +1,25 @@
+package org.moreunit.mock.elements;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.junit.jupiter.api.Test;
+
+public class TypeFacadeFactoryTest
+{
+    @Test
+    public void should_create_and_check()
+    {
+        TypeFacadeFactory factory = new TypeFacadeFactory();
+        assertFalse(factory.isTestCase(mock(ICompilationUnit.class)));
+    }
+
+    @Test
+    public void should_create_facade()
+    {
+        TypeFacadeFactory factory = new TypeFacadeFactory();
+        assertNotNull(factory.createFacade(mock(ICompilationUnit.class)));
+    }
+}
